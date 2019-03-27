302f1756fa028dcd22450ca9bf9a76dabf5edca5
Andrew <xie.andrew2235@gmail.com>
2019-03-26 22:45:57 -0700

    added if statement, forloop, and variable. variable will be the user input, which is determined by running a forloop to check if the user input matches with any of the IDs of the items available for sale. i used parseInt because we want to return an integer (please correct me if im wrong). added pseudocode on what i think are next steps

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,36 +16,43 @@ connection.connect(function (err) {
     viewTable();
 });
 // Creating a variable viewTable which will display the data stored in the products table in SQL
-var viewTable = function (){
+var viewTable = function () {
     // Connect to query, and then selects table "products" and loops through the created items, and returns them in a table
     connection.query("SELECT * FROM products", function (err, results) {
         if (err) throw err;
         for (var i = 0; i < results.length; i++) {
-            console.log("| " + results[i].id + " | " + results[i].product_name + " | " + results[i].department_name + " | " + results[i].price + " | " + results[i].stock_quantity +" |");
+            console.log("| " + results[i].id + " | " + results[i].product_name + " | " + results[i].department_name + " | " + results[i].price + " | " + results[i].stock_quantity + " |");
         }
         order();
-    });
+    })
+};
 
 // Start function which will prompt user with questions, and .then for what happens based on user response input
 var order = function () {
-    connection.query("SELECT * FROM products", function(err, results) {
+    connection.query("SELECT * FROM products", function (err, results) {
         if (err) throw err; {
-    inquirer
-    // Prompt
-        .prompt({
-            name: "itemId",
-            type: "input",
-            message: "What is the ID number of the item you would like to bid on?",
-        })
-        .then(function (answer) {
-            // based on their answer, either call the bid or the post functions
-            if (answer.itemId === "") {
-                // insert function();
-
-            }
-        }
-        )}})
-    }}
-
-// To display mySQL data on bash, we need to
-// 1. Select * from table, function, if (err) throw err
+            inquirer
+                // Prompt
+                .prompt({
+                    name: "itemId",
+                    type: "input",
+                    message: "What is the ID number of the item you would like to bid on?",
+                })
+                .then(function (answer) {
+                    // .then for response after getting user input
+                    var chosenItem;
+                    // Loop through results and create if statement: if user input matches ID of item for sale, then the variable chosenItem will equal to result[i] (whichever ID number user enters)
+                    for (var i = 0; i < results.length; i++) {
+                        // Use parseInt for integer?
+                        if (parseInt(results[i].id) === parseInt(answer.choice)) {
+                            chosenItem = results[i];
+                }
+                    // Check if user-entered quantity is greater than or equal to quantity available?
+                    // Which means we need a variable for the new quantity//find a way to update quantity on DB
+                    }
+                    // If enough quantity then sale goes through
+                }
+                // If sale does not go through, which would be our else statement. Console log.
+        else {
+    console.log("Sorry, we don't have enough in stock! Please enter a smaller amount and try again");
+    }
